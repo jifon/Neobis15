dd179b92537636507b0e3dd6a09879c2a462345a
jifon <orozbekova.kyzzhjibek@gmail.com>
2022-02-20 05:25:59 +0600

    added simple project with Rest API, Spring boot,MVC, CRUD

---
+++ src/week6/part 2/Spring-boot-tutorial/Spring-boot-tutorial/src/main/java/com/daolycodebuffer/Springboot/tutorial/model/Response.java
@@ -0,0 +1,15 @@
+package com.daolycodebuffer.Springboot.tutorial.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+    private String massage;
+    private boolean status;
+}
